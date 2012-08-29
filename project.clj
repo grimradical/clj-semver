00ba68dad4d32c21dae2d5f78548624415de2516
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-08-28 19:26:46 -0600

    Add license information
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject grimradical/clj-semver "0.1.0-SNAPSHOT"
   :description "Parsing, comparison, and manipulation of semantic version strings"
   :url "http://github.com/grimradical/clj-semver"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            :distribution :repo}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "[1.3.0,)"]]
   :profiles {:dev {:dependencies [[org.clojure/math.combinatorics "0.0.2"]]}})
