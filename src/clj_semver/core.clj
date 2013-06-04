a554b19cfd3a05075a581cffc45acec4e5c6b477
Andrew Roetker <andrew.roetker@puppetlabs.com>
2013-06-04 14:15:54 -0700

    (maint) Add invalid version number to error msg
    Prior to this commit the error message for invalid versions did not
    return the version number. This commit adds the invalid version number
    to the message.

--- src/clj_semver/core.clj
+++ src/clj_semver/core.clj
@@ -31,7 +31,7 @@
   [s]
   (let [response (re-matches pattern s)]
     (when (nil? response)
-      (throw (new IllegalArgumentException "Not a valid version format")))
+      (throw (new IllegalArgumentException (str s  "is not a valid semantic version number"))))
     response))
 
 (defn parse
